93c33fbe34264c66dc6fee14c8b7b2875e646d10
Michele1421 <michelevicino54@gmail.com>
2025-02-12 11:03:00 +0100

    Add files via upload

---
+++ Nuova cartella (3)/script.js.js
@@ -0,0 +1,22 @@
+
+       
+ 
+  const num1 = (prompt("Inserisci il primo numero:"));
+  const num2 = (prompt("Inserisci il secondo numero:"));
+
+  
+  const somma = num1 + num2;
+  const sottrazione = num1 - num2;
+  const moltiplicazione = num1 * num2;
+  const divisione = num1 / num2;
+  const potenza = num1 ** num2;
+
+  
+  console.log("I numeri da te scelti,sono:");
+  console.log(`Il valore della somma: ${somma}`);
+  console.log(`Il valore della sottrazione: ${sottrazione}`);
+  console.log(`Il valore della moltiplicazione: ${ moltiplicazione}`);
+  console.log(`Il valore della divisione: ${ divisione}`);
+  console.log(`Il valore della potenza: ${ potenza}`);
+
+
